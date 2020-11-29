4d9af13cf70e60ddc93506ace653a3cf0b9eb250
Luteranya Nyange <dieume.dev@gmail.com>
2020-11-29 19:57:44 +0300

    return all categories and their children

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Models\Traits\HasChildren;
 use App\Models\Traits\IsOrderable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
