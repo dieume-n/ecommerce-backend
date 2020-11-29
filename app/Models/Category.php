0db5ee7973e642aa644f64ef4a124120ddf83010
Luteranya Nyange <dieume.dev@gmail.com>
2020-11-29 19:31:13 +0300

    refactor parent and ordered scope to traits

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,28 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasChildren;
+use App\Models\Traits\IsOrderable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, HasChildren, IsOrderable;
 
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'slug'];
 
     public function children()
     {
         return $this->hasMany(Category::class, 'parent_id', 'id');
-    }
-
-    public function scopeParents(Builder $builder)
-    {
-        $builder->whereNull('parent_id');
-    }
-
-    public function scopeOrdered(Builder $builder, $direction = "asc")
-    {
-        $builder->orderBy('order', $direction);
     }
 }
