5a69ad06859af3140b22768fa94801b2b1df8c8e
Luteranya Nyange <dieume.dev@gmail.com>
2020-11-30 16:29:44 +0300

    dynamic product query scoping

--- app/Http/Controllers/Products/ProductController.php
+++ app/Http/Controllers/Products/ProductController.php
@@ -6,17 +6,25 @@ use App\Models\Product;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Http\Resources\ProductIndexResource;
+use App\Scoping\Scopes\CategoryScope;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::paginate(10);
+        $products = Product::withScopes($this->scopes())->paginate(10);
         return ProductIndexResource::collection($products);
     }
 
     public function show(Product $product)
     {
         return new ProductResource($product);
+    }
+
+    public function scopes()
+    {
+        return [
+            'category' => new CategoryScope()
+        ];
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Scoping\Scoper;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
@@ -12,5 +14,15 @@ class Product extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function scopeWithScopes(Builder $builder, $scopes)
+    {
+        return (new Scoper(request()))->apply($builder, $scopes);
     }
 }
