e6eadb155c4443dfd02c70acf62ced4f35c2e73c
Luteranya Nyange <dieume.dev@gmail.com>
2020-12-15 15:29:39 +0300

    product has stock information

--- tests/Unit/Models/Products/ProductTest.php
+++ tests/Unit/Models/Products/ProductTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Models\Products;
 
 use Tests\TestCase;
+use App\Models\Stock;
 use App\Models\Product;
 use App\Ecommerce\Money;
 use App\Models\Category;
@@ -61,5 +62,41 @@ class ProductTest extends TestCase
         ]);
 
         $this->assertEquals($product->formattedPrice, "$10.00");
+    }
+
+    /** @test */
+    public function it_can_check_if_product_is_in_stock()
+    {
+        $product = Product::factory()->create();
+
+        $product->variations()->save(
+            $variation = ProductVariation::factory()->create()
+        );
+
+        $variation->stocks()->save(
+            Stock::factory()->make()
+        );
+
+        $this->assertIsBool($product->inStock());
+        $this->assertTrue($product->inStock());
+    }
+
+    /** @test */
+    public function it_has_stock_count_information()
+    {
+        $product = Product::factory()->create();
+
+        $product->variations()->save(
+            $variation = ProductVariation::factory()->create()
+        );
+
+        $variation->stocks()->save(
+            Stock::factory()->make([
+                'quantity' => $quantity = 5
+            ])
+        );
+
+        $this->assertIsNumeric($product->stockCount());
+        $this->assertEquals($product->stockCount(), $quantity);
     }
 }
