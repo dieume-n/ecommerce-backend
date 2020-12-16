f9379bb692d87c32f9d5738c13b256119ed7dfcd
Luteranya Nyange <dieume.dev@gmail.com>
2020-12-16 12:38:04 +0300

    user signup

---
+++ app/Http/Controllers/Auth/SignupController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\Auth\SignupRequest;
+use App\Http\Resources\PrivateUserResource;
+
+class SignupController extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json(new PrivateUserResource($user), 201);
+    }
+}
---
+++ tests/Feature/Http/Controllers/Auth/SignupTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SignupTest extends TestCase
+{
+    /** @test */
+    public function it_requires_a_name()
+    {
+        $this->json('POST', 'api/auth/signup')
+            ->assertJsonValidationErrors(['name']);
+    }
+    /** @test */
+    public function it_requires_an_email()
+    {
+        $this->json('POST', 'api/auth/signup')
+            ->assertJsonValidationErrors(['email']);
+    }
+
+    /** @test */
+    public function it_requires_a_valid_email()
+    {
+        $this->json('POST', 'api/auth/signup', [
+            'email' => 'abc'
+        ])
+            ->assertJsonValidationErrors(['email']);
+    }
+
+    /** @test */
+    public function it_requires_a_unique_email()
+    {
+        $user = User::factory()->create();
+
+        $this->json('POST', 'api/auth/signup', [
+            'email' => $user->email
+        ])
+            ->assertJsonValidationErrors(['email']);
+    }
+    /** @test */
+    public function it_requires_a_password()
+    {
+        $this->json('POST', 'api/auth/signup')
+            ->assertJsonValidationErrors(['password']);
+    }
+
+    /** @test */
+    public function it_registers_a_user()
+    {
+        $this->json('POST', 'api/auth/signup', [
+            'name' => $name = 'John Doe',
+            'email' => $email = 'john@example.com',
+            'password' => 'secret123'
+        ]);
+        $this->assertDatabaseHas('users', [
+            'email' => $email,
+            'name' => $name
+        ]);
+    }
+
+    /** @test */
+    public function it_returns_a_user_on_signup()
+    {
+        $this->json('POST', 'api/auth/signup', [
+            'name' => $name = 'John Doe',
+            'email' => $email = 'john@example.com',
+            'password' => 'secret123'
+        ])->assertJsonFragment([
+            'email' => $email
+        ]);
+    }
+}
